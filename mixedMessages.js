6a7f2558191d898c7ffa5375fac0a62965d9cbc7
Alastair Lloyd <dex8056@aol.com>
2023-08-31 09:08:49 -0600

    qoutes added

---
+++ mixedMessages.js
@@ -0,0 +1,8 @@
+//array of quotes to be used as random messages.
+
+const quotes = [
+  “Waste no more time arguing what a good man should be. Be One. – Marcus Aurelius",
+  “It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. – Marcus Aurelius",
+  “If it is not right, do not do it, if it is not true, do not say it. – Marcus Aurelius"
+]
+
