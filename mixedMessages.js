2887baa7d917f6a81e68a03d0836d829487b6d00
Alastair Lloyd <dex8056@aol.com>
2023-08-31 09:27:30 -0600

    basic functionality for quotes

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,8 +1,18 @@
 //array of quotes to be used as random messages.
 
 const quotes = [
-  “Waste no more time arguing what a good man should be. Be One. – Marcus Aurelius",
-  “It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. – Marcus Aurelius",
-  “If it is not right, do not do it, if it is not true, do not say it. – Marcus Aurelius"
-]
+  'Waste no more time arguing what a good man should be. Be One.',
+  'It never cease to amaze me, we all love ourselves more than other people, but care more about their opinion than our own.',
+  'If it is not right, do not do it, if it is not true, do not say it.'
+];
 
+//const joke = [
+ // 'Id tell you a joke about butter but youd probably spread it',];
+
+ function randomMessage(){
+    let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    return randomQuote
+ }
+
+  console.log(randomMessage())
